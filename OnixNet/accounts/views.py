eb3954a467c39635c0fdf0a2aa539d48f6850f0d
Mark Shapirovskyy <mshapirovskyy@gmail.com>
2024-05-03 11:53:45 +0300

    Redirect to / and login when signing up

--- OnixNet/accounts/views.py
+++ OnixNet/accounts/views.py
@@ -1,9 +1,23 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import UserRegisterForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+
 
 class SignUpView(generic.CreateView):
     form_class = UserRegisterForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+    def form_valid(self, form):
+        # save the new user first
+        form.save()
+        # get the username and password
+        username = self.request.POST["username"]
+        password = self.request.POST["password1"]
+        # authenticate user then login
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return HttpResponseRedirect("/")
