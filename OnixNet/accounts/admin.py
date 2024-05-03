3d3257d3ee92648c657f62422f0348c43259a0a3
Mark Shapirovskyy <mshapirovskyy@gmail.com>
2024-05-03 13:53:06 +0300

    ADmin site

--- OnixNet/accounts/admin.py
+++ OnixNet/accounts/admin.py
@@ -4,3 +4,6 @@ from .models import User
 
 
 admin.site.register(User, UserAdmin)
+admin.site.site_header = "OnixNet"
+admin.site.site_title = "OnixNet Admin Portal"
+admin.site.index_title = "Welcome to the OnixNet Portal"
