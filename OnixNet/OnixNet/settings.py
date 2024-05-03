698f67f1f4632f53f858e514c1b0808d5de48bf8
Mark Shapirovskyy <mshapirovskyy@gmail.com>
2024-05-03 11:00:23 +0300

    Moved user model to accounts app

--- OnixNet/OnixNet/settings.py
+++ OnixNet/OnixNet/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = "website.User"
+AUTH_USER_MODEL = "accounts.User"
 
 # Redirect to home URL after login
 LOGIN_REDIRECT_URL = "/"
