be903053172670afd3aa62adc5160de02ac4043a
Mark Shapirovskyy <mshapirovskyy@gmail.com>
2024-05-06 15:47:11 +0300

    Community hub

--- OnixNet/website/urls.py
+++ OnixNet/website/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, PostDetailView
+from .views import index, PostDetailView, CommunityView
 
 from . import views
 
 urlpatterns = [
     path("", index.as_view(), name="index"),
+    path("<str:community>", CommunityView.as_view(), name="community"),
     path("<str:community>/<int:pk>/", PostDetailView.as_view(), name="article-detail"),
 ]
--- OnixNet/website/views.py
+++ OnixNet/website/views.py
@@ -1,7 +1,8 @@
 from django.utils import timezone
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Post
+from .models import Post, Community
 
 class index(ListView):
     model = Post
@@ -12,6 +13,23 @@ class index(ListView):
         context = super().get_context_data(**kwargs)
         context["now"] = timezone.now()
         return context
+
+class CommunityView(ListView):
+    model = Post
+    paginate_by = 100
+    template_name = "community.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        community = Community.objects.filter(name=self.kwargs["community"])
+        if len(community) > 0:
+            context["community"] = community[0]
+            context["exists"] = True
+        else:
+            context["exists"] = False
+        return context
+    def get_queryset(self):
+        return Post.objects.filter(community__name=self.kwargs["community"])
 
 
 class PostDetailView(DetailView):
