e39165f5a9e3ba557205cacb871351c1e20cbc8f
Mark Shapirovskyy <mshapirovskyy@gmail.com>
2024-05-09 15:48:04 +0300

    Updated readme and attachments path

--- OnixNet/website/models.py
+++ OnixNet/website/models.py
@@ -38,8 +38,8 @@ class Comment(models.Model):
 
 
 class Attachment(models.Model):
-    image = models.ImageField(upload_to="media/attachments/", null=True, default=None)
-    video = models.FileField(upload_to="media/attachments/", null=True, default=None)
+    image = models.ImageField(upload_to="attachments/", null=True, default=None)
+    video = models.FileField(upload_to="attachments/", null=True, default=None)
     parent_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="attachments")
 
     def clean(self):
