a544127eb0b928e0662fb528f2e0aa83206bd118
ExoticOnix <72964425+ExoOnix@users.noreply.github.com>
2024-05-17 21:38:58 +0300

    Update settings.py

--- OnixNet/OnixNet/settings.py
+++ OnixNet/OnixNet/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'prod_static/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
